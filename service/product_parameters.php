c4a29edd7549e4e4207094f4f587e2235ed9c23f
kemza <lubor@kemza.com>
2012-02-18 23:36:52 +0100

    Sluby serveru

--- service/product_parameters.php
+++ service/product_parameters.php
@@ -1,22 +1,59 @@
 <?php
    header('Content-Type: text/plain; charset=UTF-8');
    include "includes/website_conn.php";
+/*
 echo '   
 {
-  "products_parameters":[
+  "products_parameters":[{
     "product_parameters_declaration": [
-      { "id":1, "name":"Obsah lepku", "description":"StanovenÃ½ obsah lepku", "type":"REAL", "required":1},
-      { "id":2, "name":"SchvÃ¡leno", "description":"SchvÃ¡leno jako bezlepkovÃ©", "type":"BOOL", "required":1},
-      { "id":3, "name":"BalenÃ­", "description":"BalenÃ­ produktu", "type":"ENUM", "required":0, "options":["MalÃ©", "VelkÃ©", "StÅ™ednÃ­" ]},                
+      { "id":1, "name":"Obsah lepku", "description":"Stanovený obsah lepku", "type":"REAL", "required":1},
+      { "id":2, "name":"Schváleno", "description":"Schváleno jako bezlepkové", "type":"BOOL", "required":1},
+      { "id":3, "name":"Balení", "description":"Balení produktu", "type":"ENUM", "required":0, "options":["Malé", "Velké", "Støední" ]}                
     ],
     "product_parameters_data": [
       { "product_id":1, "parameter_id":1, "value":"10 mg"},
       { "product_id":1, "parameter_id":2, "value":"TRUE"}, 
       { "product_id":2, "parameter_id":1, "value":"120 mg"},
       { "product_id":2, "parameter_id":2, "value":"FALSE"},
-      { "product_id":2, "parameter_id":3, "value":"MalÃ©"}                     
+      { "product_id":2, "parameter_id":3, "value":"Malé"}                     
     ]
-   ]     
+   } ]     
 }
-'; 
+';
+*/
+
+  $query = 'Select id as category_id, parent as parent_category, name as category_name, description as category_description from `CATEGORIES` C';
+  /* echo $query;*/
+  $Sql_result = MySQL_Query($query);
+
+  echo  '
+{
+  "products_parameters":[{
+    "product_parameters_declaration": [';  
+  while ($record = MySQL_Fetch_Array($Sql_result)):
+    echo  '{ "id":1, "name":"Obsah lepku", "description":"Stanovený obsah lepku", "type":"REAL", "required":1},'."\n";
+    $i++;
+  endwhile;
+    echo  '{ "id":"", "name":"", "description":"", "type":"", "required":""}'."\n";
+  MySQL_FreeResult($Sql_result); 
+  
+  $query = 'Select id as id, product_id as product_id, product_parameter_id as product_parameter_id, value as product_value from `PRODUCT_PROPERTIES` PP';
+  /* echo $query;*/
+  $Sql_result = MySQL_Query($query);
+  
+  echo '     
+    ],
+    "product_parameters_data": [
+  ';
+  while ($record = MySQL_Fetch_Array($Sql_result)):
+    echo  '{ "product_id":"'.$record['product_id'].'", "parameter_id":"'.$record['product_prameter_id'].'", "value":"'.$record['product_value'].'"},'."\n";
+    $i++;
+  endwhile;
+  echo  '{ "product_id":"", "parameter_id":"", "value":""}'."\n";
+  echo '     
+    ]
+   } ]     
+}
+  ';
+  MySQL_FreeResult($Sql_result); 
 ?>
