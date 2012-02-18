ecb2aa8300ed62eb8adc84c522a7b5e64db5ef97
Marian Cerny <majo-git@pematon.com>
2012-02-18 13:49:41 +0100

    Initial commit.

---
+++ src/gadcprague/fruits/celidb/CeliDBActivity.java
@@ -0,0 +1,13 @@
+package gadcprague.fruits.celidb;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CeliDBActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
